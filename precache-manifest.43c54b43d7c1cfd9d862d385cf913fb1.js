fc5155b856f81ff8d5cfe8ed26aa686f3d20dc0d
Lillian <lillianmarlee@gmail.com>
2023-02-23 23:41:07 -0500

    Updates

---
+++ precache-manifest.43c54b43d7c1cfd9d862d385cf913fb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "299a0e1d22d28bdc9433",
+    "url": "/dog-app/static/css/main.1b22fd7a.chunk.css"
+  },
+  {
+    "revision": "299a0e1d22d28bdc9433",
+    "url": "/dog-app/static/js/main.299a0e1d.chunk.js"
+  },
+  {
+    "revision": "e417341897ed9981ae83",
+    "url": "/dog-app/static/js/1.e4173418.chunk.js"
+  },
+  {
+    "revision": "bc0bc5caa12f603a66aa",
+    "url": "/dog-app/static/js/runtime~main.bc0bc5ca.js"
+  },
+  {
+    "revision": "4c4fc146a83268ebd4d04211cdc3d51e",
+    "url": "/dog-app/index.html"
+  }
+];
